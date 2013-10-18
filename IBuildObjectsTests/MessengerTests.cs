060401725e2333ebe387e3077bf5557079593f29
unknown <elondon@EL-LAPTOPWIN7.home>
2013-10-18 19:32:19 -0400

    added folders for wpf and SL5 caliburn micro binaries and made sure the correct binaries are referenced in the samples. Also updated to the newest version of CM

--- IBuildObjectsTests/MessengerTests.cs
+++ IBuildObjectsTests/MessengerTests.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using IBuildObjects;
-using IBuildObjects;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace IBuildObjectsTests
